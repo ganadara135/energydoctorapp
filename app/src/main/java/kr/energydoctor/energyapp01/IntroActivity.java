c8465bdf5a8c84b7d2721b658255afbb3b765f86
Kcod <kcod@kcod-2.local>
2018-12-10 18:38:12 +0900

    energy doctor homepage webview android app

---
+++ app/src/main/java/kr/energydoctor/energyapp01/IntroActivity.java
@@ -0,0 +1,45 @@
+package kr.energydoctor.energyapp01;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ *
+ */
+
+public class IntroActivity extends Activity{
+
+    private Handler handler;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(0, 0);
+        }
+    };
+
+    protected void onCreate(Bundle savedlnstanceState) {
+        super.onCreate(savedlnstanceState);
+        setContentView(R.layout.activity_intro);
+        init();
+
+        handler.postDelayed(runnable, 2000);
+    }
+
+    public void init(){
+        handler = new Handler();
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        handler.removeCallbacks(runnable);
+    }
+
+
+}
